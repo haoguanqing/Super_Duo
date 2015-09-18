e00ee397c9b8ba71b04161c1254fa7e2362484e9
Guanqing <haoguanqing@live.com>
2015-09-18 04:01:04 -0400

    add widget & modify some features

--- Football_Scores/app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
@@ -44,6 +44,7 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
         update_scores();
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         final ListView score_list = (ListView) rootView.findViewById(R.id.scores_list);
+        score_list.setEmptyView(rootView.findViewById(R.id.empty_textview));
         mAdapter = new scoresAdapter(getActivity(),null,0);
         score_list.setAdapter(mAdapter);
         getLoaderManager().initLoader(SCORES_LOADER,null,this);
--- Football_Scores/app/src/main/java/barqsoft/footballscores/ViewHolder.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/ViewHolder.java
@@ -21,7 +21,7 @@ public class ViewHolder
         home_name = (TextView) view.findViewById(R.id.home_name);
         away_name = (TextView) view.findViewById(R.id.away_name);
         score     = (TextView) view.findViewById(R.id.score_textview);
-        date      = (TextView) view.findViewById(R.id.data_textview);
+        date      = (TextView) view.findViewById(R.id.date_textview);
         home_crest = (ImageView) view.findViewById(R.id.home_crest);
         away_crest = (ImageView) view.findViewById(R.id.away_crest);
     }
